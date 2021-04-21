700a786b90bc64e69a027486d676ee41c7cbee17
parkjaehoon <yukihoon12@naver.com>
2021-04-21 23:51:58 +0900

    완성

--- chatting/back/app.js
+++ chatting/back/app.js
@@ -1,45 +1,19 @@
-// const app = require("express")();
-// const http = require("http").createServer(app);
-// const cors = require("cors");
-// app.use(cors());
-// const io = require("socket.io")(http, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//   },
-// });
-
-// app.use(cors());
-
-// io.on("connection", (socket) => {
-//
-//   socket.on("test", () => {
-//     console.log(21111111111111);
-//     socket.emit("test2", {
-//       msg: "hi",
-//     });
-//   });
-//   console.log("연결");
-// });
-
-// app.listen(80, () => {
-//   console.log("server on 80 port");
-// });
-
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: "http://localhost:3000",
-    // credentials: true,
-    // transports: ["websocket", "polling"],
     methods: ["GET", "POST"],
   },
-  //   allowEIO3: true,
 });
 
 io.on("connection", (socket) => {
   console.log("connection");
   socket.on("init", (payload) => {
     console.log(payload);
+  });
+  socket.on("send message", (item) => {
+    console.log(item.name + " : " + item.message);
+    io.emit("receive message", { name: item.name, message: item.message });
   });
 });
 
